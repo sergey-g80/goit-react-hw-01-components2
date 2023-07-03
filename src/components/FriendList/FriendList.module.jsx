ed26106bac1c3bd6699c5acdd4e6d4e3e8c0589c
sergey-g80 <119445810+sergey-g80@users.noreply.github.com>
2023-07-03 23:11:13 +0300

    st4

---
+++ src/components/FriendList/FriendList.module.jsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const FriendListWrapper = styled.ul`
+    list-style: none;
+    padding: 0;
+    
+    width: min-content;
+    display: flex;
+    margin: auto;
+   
+`
